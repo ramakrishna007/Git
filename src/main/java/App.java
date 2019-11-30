67a2d76192399ad37a8e73d4cfcaf59b885041b4
ramakrishna007 <krishnakittu03@gmail.com>
2019-11-30 22:45:28 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,20 +3,22 @@ package com.xyz;
 import com.sun.org.slf4j.internal.LoggerFactory;
 import org.apache.log4j.Logger;
 
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 
 public class App {
     private static Logger log = Logger.getLogger(App.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World! Today is " + getToday().toString());
         log.info("Hello World! Today is " + getToday().toString());
+        throw new IOException();
     }
 
     public static Date getToday() {
 //                String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
-        Date today = Calendar.getInstance().getTime()
+        Date today = Calendar.getInstance().getTime();
         return today;
     }
 }
