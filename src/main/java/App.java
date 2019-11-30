84e545a887770b4c110dd7610802498102fd41f9
ramakrishna007 <krishnakittu03@gmail.com>
2019-11-30 22:12:50 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,17 @@
 package com.xyz;
 
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.apache.log4j.Logger;
+
 import java.util.Calendar;
 import java.util.Date;
 
 public class App {
+    private static Logger log = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
         System.out.println("Hello World! Today is " + getToday().toString());
+        log.info("Hello World! Today is " + getToday().toString());
     }
 
     public static Date getToday() {
